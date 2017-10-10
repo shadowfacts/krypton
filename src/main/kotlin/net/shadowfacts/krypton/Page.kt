c27e1a4bdb1d7fbf350fc54b9f2ac568bc3547f9
Shadowfacts <me@shadowfacts.net>
2017-10-10 10:27:18 -0400

    Add YAML front matter parsing

--- src/main/kotlin/net/shadowfacts/krypton/Page.kt
+++ src/main/kotlin/net/shadowfacts/krypton/Page.kt
@@ -1,5 +1,6 @@
 package net.shadowfacts.krypton
 
+import org.yaml.snakeyaml.Yaml
 import java.io.File
 
 /**
@@ -12,5 +13,20 @@ data class Page(
 
 	var input = source.readText(Charsets.UTF_8)
 	var output = krypton.config.getOutput(source)
+
+	val metadata = mutableMapOf<String, Any>()
+
+	init {
+		val parts = input.split("---")
+		if (parts.size >= 2) {
+			input = parts.drop(1).joinToString("---")
+
+			val yaml = Yaml().load(parts[1])
+			if (System.getProperty("krypton.metadata.debugFrontMatter").toBoolean()) {
+				println("Front matter for $source: $yaml")
+			}
+			metadata.putAll(yaml as Map<String, Any>)
+		}
+	}
 
 }
