400d66232ab625e0d677a4471de72f528ae7a2f8
Shadowfacts <me@shadowfacts.net>
2017-10-28 18:15:18 -0400

    Add better 404 message

--- src/main/kotlin/net/shadowfacts/krypton/util/StaticServer.kt
+++ src/main/kotlin/net/shadowfacts/krypton/util/StaticServer.kt
@@ -11,10 +11,14 @@ class StaticServer(private val root: File, port: Int): NanoHTTPD(port) {
 
 	override fun serve(session: IHTTPSession): Response {
 		val file = File(root, session.uri)
-		return if (file.isFile) {
-			newFixedFileResponse(file)
+		return if (file.exists()) {
+			if (file.isFile) {
+				newFixedFileResponse(file)
+			} else {
+				newFixedFileResponse(File(file, "index.html"))
+			}
 		} else {
-			newFixedFileResponse(File(file, "index.html"))
+			newFixedLengthResponse(Response.Status.NOT_FOUND, "text/plain", "404 Not Found\nThe file '$file' could not be found.")
 		}
 	}
 
